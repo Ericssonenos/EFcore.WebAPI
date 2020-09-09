d9405441becbdcb55ac9605ac38445d5b207e674
Ericssonenos <ericsson.eno@gmail.com>
2020-09-09 14:09:38 -0300

    teste

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace EFcore.WebAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "vjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj1", "value2" };
         }
 
         // GET api/values/5
